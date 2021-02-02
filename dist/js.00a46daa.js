2c8addd6b2edfd0c1ca30f1adbfed6ef497a5452
jacos <cyrstem@gmail.com>
2021-02-02 13:34:47 -0500

    modified:   dist/js.00a46daa.js 	modified:   dist/style.cd660723.js 	modified:   dist/style.cd660723.js.map

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -41921,7 +41921,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38623" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41945" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
